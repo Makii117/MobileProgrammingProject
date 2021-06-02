275bec7e62bf9028978dbde4f38559ad3ee0630c
Makii117 <MakiOnPc@gmail.com>
2021-06-02 21:29:55 +0200

    Added splash screen

--- app/src/main/java/com/maki/happyhour/Splash.java
+++ app/src/main/java/com/maki/happyhour/Splash.java
@@ -1,40 +1,31 @@
 package com.maki.happyhour;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import static java.lang.Thread.sleep;
 
-public class LoginFragment extends AppCompatActivity {
-
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_fragment);
+        setContentView(R.layout.activity_splash);
 
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     sleep(5000);
-                    Intent i = new Intent(LoginFragment.this,MainActivity.class);
+                    Intent i = new Intent(Splash.this,LoginFragment.class);
                     startActivity(i);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
-        thread.start();
+    thread.start();
     }
 }
-
-
