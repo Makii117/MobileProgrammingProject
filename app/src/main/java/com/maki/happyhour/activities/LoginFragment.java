b0d9c6c71cb72629083c61c439781a5b5fe3ed00
Makii117 <MakiOnPc@gmail.com>
2021-06-06 14:08:48 +0200

    Added user registration

--- app/src/main/java/com/maki/happyhour/activities/LoginFragment.java
+++ app/src/main/java/com/maki/happyhour/activities/LoginFragment.java
@@ -2,6 +2,8 @@ package com.maki.happyhour.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.maki.happyhour.R;
 
@@ -10,23 +12,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import static java.lang.Thread.sleep;
 
 public class LoginFragment extends AppCompatActivity {
-
+    Button register,login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_fragment);
-
+        register = findViewById(R.id.button);
+        login = findViewById(R.id.button2);
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    sleep(5000);
-                    Intent i = new Intent(LoginFragment.this, MainActivity.class);
-                    startActivity(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                register.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent register = new Intent(LoginFragment.this,RegisterActivity.class);
+                        startActivity(register);
+                    }
+                });
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent login = new Intent(LoginFragment.this, MainActivity.class);
+                        startActivity(login);
+                    }
+                });
             }
         });
         thread.start();
--- app/src/main/java/com/maki/happyhour/activities/RegisterActivity.java
+++ app/src/main/java/com/maki/happyhour/activities/RegisterActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +19,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.maki.happyhour.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView btn;
+    private EditText inputUsername,inputEmail,inputPassword,inputConfirmPassword;
     Button btnRegister;
     private FirebaseAuth mAuth;
     private ProgressDialog mLoadingBar;
@@ -24,69 +28,72 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_fragment);
+        setContentView(R.layout.activity_register);
 
         inputUsername = findViewById(R.id.input_user);
-        inputEmail=findViewById(R.id.input_email);
+        inputEmail = findViewById(R.id.input_email);
         inputPassword = findViewById(R.id.input_pass);
-        inputConfirmPassword=findViewById(R.id.input_confirm_pass);
+        inputConfirmPassword = findViewById(R.id.input_confirm_pass);
 
-        mAuth=FirebaseAuth.getInstance();
-        mLoadingBar=new ProgressDialog(RegisterActivity.this);
+        mAuth = FirebaseAuth.getInstance();
+        mLoadingBar = new ProgressDialog(RegisterActivity.this);
 
-        btnRegister=findViewById(R.id.btn_register);
-        btnRegister.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View v){
-               checkCredentials();
-           }
+        btnRegister = findViewById(R.id.btn_register);
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCredentials();
+            }
 
         });
-
-        private void checkCredentials(){
-            String username=inputUsername.getText().toString();
+    }
+        private void checkCredentials() {
+            String username = inputUsername.getText().toString();
             String email = inputEmail.getText().toString();
             String password = inputPassword.getText().toString();
-            String confirmPass= inputConfirmPassword.getText().toString();
-        }
-
-        if(username.isEmpty()){
-            showError(inputUsername,"Invalid username");
-        }else if(email.isEmpty() || !email.contains("@")){
-            showError(inputEmail,"Invalid email");
-        }else if(password.isEmpty() || !email.contains("@")){
-            showError(inputPassword,"Invalid email");
-        }else if(confirmPass.isEmpty() || !email.contains("@")){
-            showError(inputConfirmPassword,"Invalid email");
-        }else{
-            mLoadingBar.setTitle("Register Successful");
-            mLoadingBar.setMessage("Checking credentials, please wait");
-            mLoadingBar.setCanceledOnTouchOutside(false);
-            mLoadingBar.show();
+            String confirmPass = inputConfirmPassword.getText().toString();
 
 
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(RegisterActivity.this, "Registered Successful", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
+            if (username.isEmpty()) {
+                showError(inputUsername, "Invalid username");
+            } else if (email.isEmpty() || !email.contains("@")) {
+                showError(inputEmail, "Invalid email");
+            } else if (password.isEmpty() || password.length()<7) {
+                showError(inputPassword, "Invalid email");
+            } else if (confirmPass.isEmpty() || !confirmPass.equals(password)) {
+                showError(inputConfirmPassword, "Invalid email");
+            } else {
+                mLoadingBar.setTitle("Register Successful");
+                mLoadingBar.setMessage("Checking credentials, please wait");
+                mLoadingBar.setCanceledOnTouchOutside(false);
+                mLoadingBar.show();
 
-                    }else{
-                        Toast.makeText(RegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registered Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-            });
+                });
 
 
+            }
         }
-
-
-
+    private void showError(EditText input,String s) {
+        input.setError(s);
+        input.requestFocus();
+    }
 
 
 
     }
-}
