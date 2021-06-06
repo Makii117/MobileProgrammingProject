8f7aa8270636dcb9faa940134b9cb36e0cbbd0f1
Makii117 <MakiOnPc@gmail.com>
2021-06-06 23:59:06 +0200

    User info

--- app/src/main/java/com/maki/happyhour/fragments/ProfileFragment.java
+++ app/src/main/java/com/maki/happyhour/fragments/ProfileFragment.java
@@ -1,15 +1,19 @@
 package com.maki.happyhour.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.maki.happyhour.R;
 import com.maki.happyhour.activities.LoginActivity;
 import com.maki.happyhour.activities.MainActivity;
@@ -21,6 +25,9 @@ import androidx.fragment.app.Fragment;
 public class ProfileFragment extends Fragment implements View.OnClickListener {
     View view;
     FirebaseAuth mAuth;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    TextView name,mail,phone,location;
+    ImageView userImg;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,6 +35,24 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         Button sign_out=(Button) view.findViewById(R.id.sign_out);
         sign_out.setOnClickListener(this);
         mAuth=FirebaseAuth.getInstance();
+
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String Uname = user.getDisplayName();
+            String email = user.getEmail();
+            //Uri photoUrl = user.getPhotoUrl();
+            String Uphone = user.getPhoneNumber();
+            //String location = user.getLocation();
+            name=(TextView)view.findViewById(R.id.name_surname);
+            name.setText(Uname);
+            mail=(TextView)view.findViewById(R.id.user_email);
+            mail.setText(email);
+            phone=(TextView)view.findViewById(R.id.phone_num);
+            phone.setText(Uphone);
+            location=(TextView)view.findViewById(R.id.last_loc);
+            location.setText("Last Location");
+        }
+
 
         return view;
     }
